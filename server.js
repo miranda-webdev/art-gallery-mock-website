875085710c3c1847136ef34feca88d3d8c8fa688
Miranda <miranda@greenfields.io>
2019-05-02 20:31:56 -0700

    Foundation for creating React app with express backend set up

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// create a GET route
+app.get('/express_backend', (req, res) => {
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+});
+
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
+
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
