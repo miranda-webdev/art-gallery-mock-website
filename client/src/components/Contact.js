1b87f9151f02e6d4254d1052f6fc7e8d3462de4c
Miranda <miranda@greenfields.io>
2019-05-07 15:11:05 -0700

    Changes to divs for smaller screen sizes

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -4,9 +4,11 @@ function Contact () {
     return (
         <div className="Contact">
             <div className="blueDiv Contact-ad">
-                <h1>Want a website like this?</h1>
-                <p>I can customize it to your style and your needs!</p>
-                <p>If there's something else you're looking for just let me know and we can go over some options!</p>
+                <div className="Contact-ad-info">
+                    <h1>Want a website like this?</h1>
+                    <p>I can customize it to your style and your needs!</p>
+                    <p>If there's something else you're looking for just let me know and we can go over some options!</p>
+                </div>
             </div>
             <div className="redDiv Contact-div">
                 <h2 className="Contact-header">Contact Me!</h2>
@@ -14,7 +16,7 @@ function Contact () {
                     <p>Email: miranda.manriquez.dev@gmail.com</p>
                     <p>Cell: ( 480 ) 249 - 5816</p>
                     <p>Hours: 9am - 6pm Arizona Time</p>
-                    <p>Website: <a href="https://www.miranda-manriquez.dev/" rel="noopener noreferrer" to target="_blank" className="Contact-link">miranda-manriquez.dev</a></p>
+                    <p>Website: <a href="https://www.miranda-manriquez.dev/" rel="noopener noreferrer" target="_blank" className="Contact-link">miranda-manriquez.dev</a></p>
                 </div>
             </div>
         </div>
