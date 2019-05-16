bb85f2e14a73d62d138e8fddb534b77c994c9baa
Miranda <codingwith.miranda@gmail.com>
2019-05-16 12:02:57 -0700

    Took out colored divs in About Page, and rearranged layout and added transparent borders to nav links

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,21 +1,23 @@
 import React from 'react';
 
 function About() {
-    return (
-        <div className="About">
-            <div className="About--img-container">
-               <img src={require('../images/max-van-den-oetelaar-1150510-unsplash.jpg')} alt=""/>
-               <div className="About--text-top redDiv">
-                    Et sit nostrud Lorem eiusmod aute nulla eiusmod ut velit in deserunt minim aliqua. Lorem anim ea esse enim enim sit nostrud. Excepteur mollit eu ea fugiat minim nostrud officia. Laborum sit dolore sunt commodo commodo sit minim labore velit. Adipisicing sint proident excepteur officia occaecat. 
-                </div>
-                <div className="About--text-right blueDiv">
-                    Et sit nostrud Lorem eiusmod aute nulla eiusmod ut velit in deserunt minim aliqua. Lorem anim ea esse enim enim sit nostrud. Excepteur mollit eu ea fugiat minim nostrud officia. Laborum sit dolore sunt commodo commodo sit minim labore velit. Adipisicing sint proident excepteur officia occaecat. 
-                </div>
-                <div className="About--text-left yellowDiv">
-                    Et sit nostrud Lorem eiusmod aute nulla eiusmod ut velit in deserunt minim aliqua. Lorem anim ea esse enim enim sit nostrud. Excepteur mollit eu ea fugiat minim nostrud officia. Laborum sit dolore sunt commodo commodo sit minim labore velit. Adipisicing sint proident excepteur officia occaecat. 
-                </div>
-            </div>
-        </div>
+    return ( 
+    <div className = "About" >
+        <div className = "About--img-container" >
+            <img src = {
+                require('../images/max-van-den-oetelaar-1150510-unsplash.jpg')
+            } alt = "" / >
+        <div className = "About--text-top" >
+            Et sit nostrud Lorem eiusmod aute nulla eiusmod ut velit in deserunt minim aliqua.Lorem anim ea esse enim enim sit nostrud.Excepteur mollit eu ea fugiat minim nostrud officia.Laborum sit dolore sunt commodo commodo sit minim labore velit.Adipisicing sint proident excepteur officia occaecat. 
+        </div> 
+        <div className = "About--text-right" >
+            Et sit nostrud Lorem eiusmod aute nulla eiusmod ut velit in deserunt minim aliqua.Lorem anim ea esse enim enim sit nostrud.Excepteur mollit eu ea fugiat minim nostrud officia.Laborum sit dolore sunt commodo commodo sit minim labore velit.Adipisicing sint proident excepteur officia occaecat. 
+        </div> 
+        <div className = "About--text-left" >
+            Et sit nostrud Lorem eiusmod aute nulla eiusmod ut velit in deserunt minim aliqua.Lorem anim ea esse enim enim sit nostrud.Excepteur mollit eu ea fugiat minim nostrud officia.Laborum sit dolore sunt commodo commodo sit minim labore velit.Adipisicing sint proident excepteur officia occaecat. 
+        </div> 
+        </div> 
+    </div>
     )
 }
 
