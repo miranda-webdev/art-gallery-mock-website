9e7b708c85251fb906b00d322d5bcb431f203ce8
Miranda <miranda.manriquez.dev@gmail.com>
2019-08-09 09:27:46 -0700

    Restarting with Material UI

---
+++ client/src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Typography from '@material-ui/core/Typography';
+
+function About() {
+    return ( 
+    <div className = "About" >
+        <Typography variant="h2">About Page</Typography>
+    </div>
+    )
+}
+
+export default About;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Typography from '@material-ui/core/Typography';
+
+function Home(){
+    return(
+        <div>
+            <Typography variant="h1">Art Gallery Home Page</Typography>
+        </div>
+    )
+}
+
+export default Home;
